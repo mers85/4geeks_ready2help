a658e5247a22e00198c826b1358a5c513776dbc7
mers85 <mers85@gmail.com>
2021-03-31 17:49:09 +0000

    migraciones basicas user y organizaciones

--- src/api/models.py
+++ src/api/models.py
@@ -3,17 +3,73 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    email = db.Column(db.String(256), unique=True, nullable=False)
+    password = db.Column(db.String(256), unique=False, nullable=False)
+    is_admin = db.Column(db.Boolean, default=False)
+    
+    organization_id = db.Column(db.Integer, db.ForeignKey('organizations.id'), nullable=True)
+    organization = db.relationship("Organization", back_populates="users")
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<User %r>' % self.id
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
-            # do not serialize the password, its a security breach
+            "organization_id": self.organization_id
         }
+
+class Organization(db.Model):
+    __tablename__ = "organizations"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(256), unique=False, nullable=False)
+    email = db.Column(db.String(256), unique=False, nullable=False)
+    address = db.Column(db.String(256), unique=False, nullable=True)
+    zipcode = db.Column(db.String(256), unique=False, nullable=True)
+    phone = db.Column(db.String(256), unique=False, nullable=True)
+
+    users = db.relationship("User", back_populates="organization")
+
+    def __repr__(self):
+        return '<Organization %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "email": self.email,
+            "address": self.address,
+            "zipcode": self.zipcode,
+            "phone": self.phone
+        }
+
+# class Person(db.Model):
+#     __tablename__ = "persons"
+#     id = db.Column(db.Integer, primary_key=True)
+#     id_user = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String(40), unique=False, nullable=False)
+#     lastname = db.Column(db.String(40), unique=False, nullable=False)
+#     email = db.Column(db.String(120), unique=False, nullable=False)
+#     address = db.Column(db.String(120), unique=False, nullable=False)
+#     zipcode = db.Column(db.String(8), unique=False, nullable=False)
+#     phone = db.Column(db.String(15), unique=False, nullable=False)
+
+#     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+#     user = db.relationship("User", back_populates="person")
+
+#     def __repr__(self):
+#         return '<Person %r>' % self.id
+
+#     def serialize(self):
+#         return {
+#             "id": self.id,
+#             "name": self.name,
+#             "lastname": self.lastname,
+#             "email": self.email,
+#             "address": self.address,
+#             "zipcode": self.zipcode,
+#             "phone": self.phone
+#         }
