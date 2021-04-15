490b6f7901d5b113f825d2fabe81a1a5d10f09ac
mers85 <mers85@gmail.com>
2021-04-15 11:56:55 +0000

    eliminada librerias sin utilizar de logout

--- src/front/js/pages/logout.js
+++ src/front/js/pages/logout.js
@@ -2,14 +2,7 @@ import React, { useState, useContext } from "react";
 import { Context } from "../store/appContext";
 import { Link, useHistory } from "react-router-dom";
 
-import Grid from "@material-ui/core/Grid";
-import SimpleReactValidator from "simple-react-validator";
 import { toast } from "react-toastify";
-import TextField from "@material-ui/core/TextField";
-import Button from "@material-ui/core/Button";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import Checkbox from "@material-ui/core/Checkbox";
-import "../../styles/formularioBase.scss";
 
 export const LogOut = () => {
 	const { actions } = useContext(Context);
