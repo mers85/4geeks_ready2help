b3a9d328305a0ec2c7309577b09101c5373e9d12
mers85 <mers85@gmail.com>
2021-05-03 20:30:18 +0000

    añadido estilos responsive al formulario de detalles del usuario -person-

--- src/front/js/pages/registerPerson.js
+++ src/front/js/pages/registerPerson.js
@@ -7,9 +7,7 @@ import { toast } from "react-toastify";
 import SimpleReactValidator from "simple-react-validator";
 
 import FixedAlert from "../component/fixedAlert";
-import Grid from "@material-ui/core/Grid";
-import TextField from "@material-ui/core/TextField";
-import Button from "@material-ui/core/Button";
+import "../../styles/formularioBase2.scss";
 
 export const RegisterPerson = props => {
 	const [notification, setNotification] = useState("");
@@ -109,134 +107,115 @@ export const RegisterPerson = props => {
 	};
 
 	return (
-		<Grid className="projectWrapper">
+		<div className="container formulario-base py-5 mt-5 px-0">
 			<div className="row mx-auto">
-				<div className="col-8 mx-auto">
+				<div className="col-sm-12 col-md-10 mx-auto">
 					{notification ? <FixedAlert color="info" message={notification} /> : ""}
+					<div className="card p-md-4 py-4 border-0 shadow">
+						<h2 className="card-title text-center p-3">Datos Complementarios</h2>
+						<small className="card-subtitle p-2 text-center">
+							Sólo debes cumplimentar estos datos si deseas realizar una donación y/o querer participar
+							como voluntario.
+						</small>
+						<div className="card-body mx-0">
+							<form onSubmit={submitForm}>
+								<div className="form-row py-3">
+									<div className="form-group col-sm-12 col-md-12 textOnInput">
+										<label className="textLabel">Nombre</label>
+										<input
+											type="text"
+											className="form-control"
+											placeholder="Nombre"
+											name="name"
+											defaultValue={value.name}
+											onBlur={e => changeHandler(e)}
+											onChange={e => changeHandler(e)}
+										/>
+										{validator.message("Name", value.name, "required:name")}
+									</div>
+								</div>
+								<div className="form-row pb-3">
+									<div className="form-group col-sm-12 col-md-12 textOnInput">
+										<label className="textLabel">Apellidos</label>
+										<input
+											type="text"
+											className="form-control"
+											placeholder="Apellidos"
+											name="lastname"
+											defaultValue={value.lastname}
+											onBlur={e => changeHandler(e)}
+											onChange={e => changeHandler(e)}
+										/>
+										{validator.message("lastname", value.lastname, "required:lastname")}
+									</div>
+								</div>
+								<div className="form-row pb-3">
+									<div className="form-group col-md-12 textOnInput">
+										<label className="textLabel">Dirección</label>
+										<input
+											type="text"
+											className="form-control"
+											placeholder="Dirreción"
+											name="address"
+											defaultValue={value.address}
+											onBlur={e => changeHandler(e)}
+											onChange={e => changeHandler(e)}
+										/>
+										{validator.message("address", value.address, "required:address")}
+									</div>
+								</div>
+								<div className="form-row ">
+									<div className="form-group col-sm-12 col-md-6 textOnInput pb-3">
+										<label className="textLabel">Código Postal</label>
+										<input
+											type="text"
+											className="form-control"
+											placeholder="Código Postal"
+											name="zipcode"
+											defaultValue={value.zipcode}
+											onBlur={e => changeHandler(e)}
+											onChange={e => changeHandler(e)}
+										/>
+										{validator.message("zipcode", value.zipcode, "required:zipcode")}
+									</div>
+									<div className="form-group col-sm-12 col-md-6 textOnInput pb-3">
+										<label className="textLabel">Email de contacto</label>
+										<input
+											type="email"
+											className="form-control"
+											placeholder="Email de contacto"
+											name="email"
+											defaultValue={value.email}
+											onBlur={e => changeHandler(e)}
+											onChange={e => changeHandler(e)}
+										/>
+										{validator.message("email", value.email, "required:email")}
+									</div>
+								</div>
+								<div className="form-row pb-2">
+									<div className="form-group col-sm-12 col-md-6 textOnInput pb-3">
+										<label className="textLabel">Teléfono</label>
+										<input
+											type="text"
+											className="form-control"
+											placeholder="Teléfono"
+											name="phone"
+											defaultValue={value.phone}
+											onBlur={e => changeHandler(e)}
+											onChange={e => changeHandler(e)}
+										/>
+										{validator.message("phone", value.phone, "required:phone")}
+									</div>
+								</div>
+
+								<button type="submit" className="btn button-green btn-md btn-block">
+									Guardar datos complementarios
+								</button>
+							</form>
+						</div>
+					</div>
 				</div>
-				<Grid className="projectForm">
-					<h2>Datos Complementarios</h2>
-					<p>Completa tu perfil</p>
-					<form onSubmit={submitForm}>
-						<Grid container spacing={3}>
-							<Grid item xs={12}>
-								<TextField
-									className="inputOutline"
-									fullWidth
-									placeholder="Nombre"
-									value={value.name}
-									variant="outlined"
-									name="name"
-									type="text"
-									label="Nombre"
-									InputLabelProps={{
-										shrink: true
-									}}
-									onBlur={e => changeHandler(e)}
-									onChange={e => changeHandler(e)}
-								/>
-								{validator.message("name", value.name, "required:name")}
-							</Grid>
-							<Grid item xs={12}>
-								<TextField
-									className="inputOutline"
-									fullWidth
-									placeholder="Apellidos"
-									value={value.lastname}
-									variant="outlined"
-									name="lastname"
-									type="text"
-									label="Apellidos"
-									InputLabelProps={{
-										shrink: true
-									}}
-									onBlur={e => changeHandler(e)}
-									onChange={e => changeHandler(e)}
-								/>
-								{validator.message("lastname", value.lastname, "required:lastname")}
-							</Grid>
-							<Grid item xs={12}>
-								<TextField
-									className="inputOutline"
-									fullWidth
-									placeholder="Dirección"
-									value={value.address}
-									variant="outlined"
-									name="address"
-									type="text"
-									label="Dirección"
-									InputLabelProps={{
-										shrink: true
-									}}
-									onBlur={e => changeHandler(e)}
-									onChange={e => changeHandler(e)}
-								/>
-								{validator.message("address", value.address, "required:address")}
-							</Grid>
-							<Grid item xs={6}>
-								<TextField
-									className="inputOutline"
-									fullWidth
-									placeholder="Código Postal"
-									value={value.zipcode}
-									variant="outlined"
-									name="zipcode"
-									type="text"
-									label="Código Postal"
-									InputLabelProps={{
-										shrink: true
-									}}
-									onBlur={e => changeHandler(e)}
-									onChange={e => changeHandler(e)}
-								/>
-								{validator.message("zipcode", value.zipcode, "required:zipcode")}
-							</Grid>
-							<Grid item xs={6}>
-								<TextField
-									className="inputOutline"
-									fullWidth
-									placeholder="Email de contacto"
-									value={value.email}
-									variant="outlined"
-									name="email"
-									label="Email de contacto"
-									InputLabelProps={{
-										shrink: true
-									}}
-									onBlur={e => changeHandler(e)}
-									onChange={e => changeHandler(e)}
-								/>
-								{validator.message("email", value.email, "required:email")}
-							</Grid>
-							<Grid item xs={6}>
-								<TextField
-									className="inputOutline"
-									fullWidth
-									placeholder="Teléfono"
-									value={value.phone}
-									variant="outlined"
-									name="phone"
-									type="text"
-									label="Teléfono"
-									InputLabelProps={{
-										shrink: true
-									}}
-									onBlur={e => changeHandler(e)}
-									onChange={e => changeHandler(e)}
-								/>
-								{validator.message("phone", value.phone, "required:phone")}
-							</Grid>
-							<Grid item xs={12}>
-								<Grid className="formFooter">
-									<Button fullWidth className="cBtnTheme" type="submit">
-										Completar Perfil
-									</Button>
-								</Grid>
-							</Grid>
-						</Grid>
-					</form>
-				</Grid>
 			</div>
-		</Grid>
+		</div>
 	);
 };
