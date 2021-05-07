6d617598629753710bd6a5f8de0d897d6960f51a
mers85 <mers85@gmail.com>
2021-05-07 11:29:09 +0000

    fixed style projects

--- src/data_inicial.py
+++ src/data_inicial.py
@@ -49,43 +49,43 @@ data = {
     
     "Project": [
         {
-            "description": "Proyecto 1",
+            "description": "Proyecto 1: At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.",
             "money_needed": 70000,
             "organization_id": 1,
             "people_needed": 2,
             "status": "draft",
-            "subtitle": "Proyecto 1",
-            "title": "Proyecto 1",
+            "subtitle": "Proyecto 1: Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.",
+            "title": "Proyecto 1: Neque porro quisquam est qui dolorem ipsum quia dolor sit amet",
             "total_donated":0
         },
         {
-            "description": "Proyecto 2",
+            "description": "Proyecto 2: At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.",
             "money_needed": 60000,
             "organization_id": 1,
             "people_needed": 60,
             "status": "draft",
-            "subtitle": "Proyecto 2",
-            "title": "Proyecto 2",
+            "subtitle": "Proyecto 2: Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.",
+            "title": "Proyecto 2: Neque porro quisquam est qui dolorem ipsum quia dolor sit amet",
             "total_donated":0
         },
         {
-            "description": "Proyecto 3",
+            "description": "Proyecto 3: At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.",
             "money_needed": 50000,
             "organization_id": 1,
             "people_needed": 50,
             "status": "draft",
-            "subtitle": "Proyecto 3",
-            "title": "Proyecto 3",
+            "subtitle": "Proyecto 3: Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.",
+            "title": "Proyecto 3: Neque porro quisquam est qui dolorem ipsum quia dolor sit amet",
             "total_donated":0
         },
         {
-            "description": "Proyecto 4",
+            "description": "Proyecto 4: At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.",
             "money_needed": 20000,
             "organization_id": 1,
             "people_needed": 20,
             "status": "draft",
-            "subtitle": "Proyecto 4",
-            "title": "Proyecto 4",
+            "subtitle": "Proyecto 4: Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. ",
+            "title": "Proyecto 4: Neque porro quisquam est qui dolorem ipsum quia dolor sit amet",
             "total_donated":0
         }
     ]
