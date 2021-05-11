514181b36a96a5467cfc232a6411b2ed503ad04d
mers85 <mers85@gmail.com>
2021-05-11 17:26:48 +0000

    data

--- src/data_inicial.py
+++ src/data_inicial.py
@@ -50,7 +50,7 @@ data = {
     "Project": [
         {
             "description": "Arca de Noé tiene un especial empeño en proteger aquellos tipos de animales que son peor tratados por la sociedad como pueden ser los galgos, gatos negros, gatos y perros con enfermedades crónicas o con una muy avanzada edad. Unas horas al día o a la semana de nuestro tiempo libre para ayudarlos no nos supone mucho esfuerzo y es una labor bonita y gratificante. ¡Eso os lo aseguramos! En estos momentos necesitamos más que nunca tu aporte para ayudarnos a mantener en buenas condiciones los animales. Nos encantaría contar con tu ayuda como voluntario para ello deberás rellenar sus datos de perfil en la plataforma solicitando tu participación Gracias por tu aportación y por permitir que sigamos con nuestra labor",
-            "money_needed": 5700,
+            "money_needed": 6700,
             "organization_id": 1,
             "people_needed": 4,
             "status": "draft",
@@ -80,7 +80,7 @@ data = {
         },
         {
             "description": "Somos una asociación sin ánimo de lucro dedicada a la confección de pelucas solidarias para personas que pierden el pelo debido a una enfermedad. Nuestro coste de producción es de tan solo 300€ cuando una peluca de pelo natural hecha a medida puede valer entre 1200 y 3000€. «Ya no estoy enferma» es una de las frases que más nos dicen cuando se colocan la peluca. Recibir una peluca de pelo natural  a medida provoca un gran impacto en la vida de las personas a las que ayudamos. Nuestras pelucas no curan ni el cáncer ni la alopecia, pero ayudan a mejorar la autoestima y como consecuencia aumentan las ganas de luchar y seguir adelante. Este año esta siendo difícil para todos, de una forma u otra todos estamos viviendo momentos difíciles. Debido a la crisis sanitaria que estamos viviendo hemos perdido el 90% de nuestros ingresos y nuestras familias nos necesitan más que nunca. Con una aportación mínima puedes ayudarnos a hacer felices a niños y adultos que además de la enfermedad están sufriendo las consecuencias económica de la crisis del COVID-19. Con la ayuda de todos, podremos donar 5 pelucas y seguiremos FABRICANDO SONRISAS!",
-            "money_needed": 2000,
+            "money_needed": 8000,
             "organization_id": 1,
             "people_needed": 0,
             "status": "draft",
