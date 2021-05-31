37c2db78801e40fa17db015122c5730cb96c5f56
mers85 <mers85@gmail.com>
2021-05-31 19:47:40 +0000

    data

--- src/data_inicial.py
+++ src/data_inicial.py
@@ -50,7 +50,7 @@ data = {
     "Category": [
         {
             "id": 1,
-            "name": "Salud"
+            "name": "Salud y bienestar"
         },
         {
             "id": 2,
@@ -85,7 +85,7 @@ data = {
             "featured_image_aws_url": "https://ready2help.s3.eu-west-3.amazonaws.com/dog-1563896_1920.jpg",
             "title": "Asociación Arca de Noé: colabora con nuestros animales",
             "total_donated":0
-        },
+         },
         {
             "description": "Nuestros inicios tuvieron lugar en el año 2018 y, desde entonces, hemos centrado nuestra atención en el programa de ocio. Cada fin de semana hemos realizado actividades de ocio y tiempo libre, contribuyendo a mejorar la autonomía y habilidades sociales de este colectivo. Dos años después, tenemos un lugar donde tener un punto de encuentro para poder  intercambiar experiencias, disfrutar y aprender. Sin embargo, necesitamos hacer frente a diversos gastos, como son luz, agua, mantenimiento del local, tasas y licencias entre otros. Para continuar necesitamos tu colaboración. Muchas gracias por vuestra colaboración, sin vuestra ayuda este sueño no sería posible. ",
             "money_needed": 3800,
@@ -96,6 +96,7 @@ data = {
             "featured_image_aws_url": "https://ready2help.s3.eu-west-3.amazonaws.com/girl-6203126_1920.jpg",
             "title": "Asociación Holanda: Ayúdanos a conseguir un local",
             "total_donated":0
+            
         },
         {
             "description": "El Alzheimer puede acabar con recuerdos muy importantes, como el nombre de sus hijos, pero la Banda Sonora de sus vidas puede permanecer, y ayudar a mejorar su estado. Cada 3 segundos una persona es diagnosticada de demencia en el Mundo, más de 40 Millones de personas afectadas, y siendo la previsión superar los 135 Millones en el año 2050. Es momento de actuar. El Alzheimer acaba con muchos recuerdos importantes, pero la música de sus vidas puede permanecer, y ayudar a mejorar su estado. Puedes sumarte a nuestra iniciativa donando la cantidad que puedas o con otras formas de colaboración. Gracias a las donaciones que recibimos, podemos seguir con nuestra labor de concienciación y sensibilización social, así como posibilitar Becas de Formación para Cuidadores Familiares y otras necesidades.",
@@ -104,8 +105,10 @@ data = {
             "people_needed": 0,
             "status": "draft",
             "subtitle": "El Alzheimer puede acabar con recuerdos muy importantes, pero la Banda Sonora de sus vidas puede permanecer",
+            "featured_image_aws_url": "https://ready2help.s3.eu-west-3.amazonaws.com/hands-piano.jpg",
             "title": "Musica para Despertar – Música para convivir con el Alzheimer",
             "total_donated":0
+           
         },
         {
             "description": "Somos una asociación sin ánimo de lucro dedicada a la confección de pelucas solidarias para personas que pierden el pelo debido a una enfermedad. Nuestro coste de producción es de tan solo 300€ cuando una peluca de pelo natural hecha a medida puede valer entre 1200 y 3000€. «Ya no estoy enferma» es una de las frases que más nos dicen cuando se colocan la peluca. Recibir una peluca de pelo natural  a medida provoca un gran impacto en la vida de las personas a las que ayudamos. Nuestras pelucas no curan ni el cáncer ni la alopecia, pero ayudan a mejorar la autoestima y como consecuencia aumentan las ganas de luchar y seguir adelante. Este año esta siendo difícil para todos, de una forma u otra todos estamos viviendo momentos difíciles. Debido a la crisis sanitaria que estamos viviendo hemos perdido el 90% de nuestros ingresos y nuestras familias nos necesitan más que nunca. Con una aportación mínima puedes ayudarnos a hacer felices a niños y adultos que además de la enfermedad están sufriendo las consecuencias económica de la crisis del COVID-19. Con la ayuda de todos, podremos donar 5 pelucas y seguiremos FABRICANDO SONRISAS!",
@@ -114,8 +117,28 @@ data = {
             "people_needed": 0,
             "status": "draft",
             "subtitle": "Regala una sonrisa contra el cáncer ¡Ayúdanos a fabricar 5 pelucas!",
+            "featured_image_aws_url": "https://ready2help.s3.eu-west-3.amazonaws.com/mechon_solidario.jpg",
             "title": "Mechones Solidarios – Regala una sonrisa contra el cáncer",
             "total_donated":0
+            
+        }
+    ],
+    "project_categories" :[
+        {
+            "category_id": 3,
+            "project_id": 1
+        },
+        {
+            "category_id": 1,
+            "project_id": 2
+        },
+        {
+            "category_id": 1,
+            "project_id": 3
+        },
+        {
+            "category_id": 1,
+            "project_id": 4
         }
     ]
 }
